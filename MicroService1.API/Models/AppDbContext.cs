14b358179b9d14620284618d76c898245c3362aa
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-09-14 14:15:33 +0300

    localhost,1433 veritabanına code-First ile veritabanı oluşturuldu

---
+++ MicroService1.API/Models/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicroService1.API.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions opt):base(opt)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
