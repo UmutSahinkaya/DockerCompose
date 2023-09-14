4cb20be50f5bb34900e01499558a68d81d536ee6
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-09-14 14:19:32 +0300

    Test Controller

---
+++ MicroService1.API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using MicroService1.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroService1.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Save()
+        {
+           _context.Products.Add(new Product {Name = "Test" });
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
